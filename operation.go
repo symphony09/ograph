0dd650e211d1c3ea8f8c220485bcea0930af956e
symphony09 <sunfish69@163.com>
2024-05-21 19:56:33 +0800

    feat: add branch reagister op

--- operation.go
+++ operation.go
@@ -29,3 +29,30 @@ var Then = func(nextElements ...*Element) Op {
 		}
 	}
 }
+
+// Register(a, ograph.Branch(b, c, d)) => a->b->c->d
+var Branch = func(elements ...*Element) Op {
+	return func(pipeline *Pipeline, element *Element) {
+		if len(elements) == 0 {
+			return
+		}
+
+		var prev, next *Element
+
+		prev = element
+
+		for i := range elements {
+			next = elements[i]
+
+			if pipeline.elements[next.Name] == nil {
+				pipeline.Register(next)
+			}
+
+			if pipeline.elements[next.Name] == next {
+				pipeline.graph.AddEdge(prev.Name, next.Name)
+			}
+
+			prev = next
+		}
+	}
+}
